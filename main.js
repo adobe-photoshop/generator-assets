f1929fbefc257a9c748c7586e4cac7b6ef2fafa8
Ian Wehrman <wehrman@adobe.com>
2014-03-21 10:10:09 -0700

    JSDoc for the main module

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
+ * Copyright (c) 2014 Adobe Systems Incorporated. All rights reserved.
  *  
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"), 
@@ -29,7 +29,9 @@
         RenderManager = require("./lib/rendermanager"),
         AssetManager = require("./lib/assetmanager");
 
-    var _documentManager,
+    var _generator,
+        _config,
+        _documentManager,
         _stateManager,
         _renderManager;
 
@@ -38,46 +40,69 @@
     var _waitingDocuments = {},
         _canceledDocuments = {};
 
+    /**
+     * Enable asset generation for the given Document ID, causing all annotated
+     * assets in the given document to be regenerated.
+     * 
+     * @param {!number} id The document ID for which asset generation should be enabled.
+     */
+    function _enableAssetGeneration(id) {
+        if (_waitingDocuments.hasOwnProperty(id)) {
+            return;
+        }
+
+        var documentPromise = _documentManager.getDocument(id);
+        
+        _waitingDocuments[id] = documentPromise;
+
+        documentPromise.done(function (document) {
+            delete _waitingDocuments[id];
+
+            if (_canceledDocuments.hasOwnProperty(id)) {
+                delete _canceledDocuments[id];
+            } else {
+                if (!_assetManagers.hasOwnProperty(id)) {
+                    _assetManagers[id] = new AssetManager(_generator, _config, document, _renderManager);
+
+                    document.on("close", function () {
+                        _assetManagers[id].pause();
+                        delete _assetManagers[id];
+                    }.bind(this));
+                }
+                _assetManagers[id].unpause();
+            }
+        });
+    }
+
+    /**
+     * Disable asset generation for the given Document ID, halting any asset
+     * rending in progress.
+     * 
+     * @param {!number} id The document ID for which asset generation should be disabled.
+     */
+    function _disableAssetGeneration(id) {
+        if (_waitingDocuments.hasOwnProperty(id)) {
+            _canceledDocuments[id] = true;
+        } else {
+            _assetManagers[id].pause();
+        }
+    }
+
+    /**
+     * Initialize the Assets plugin.
+     * 
+     * @param {Generator} generator The Generator instance for this plugin.
+     * @param {object} config Configuration options for this plugin.
+     */
     function init(generator, config) {
+        _generator = generator;
+        _config = config;
         _documentManager = new DocumentManager(generator, config);
         _stateManager = new StateManager(generator, config);
         _renderManager = new RenderManager(generator, config);
 
-        _stateManager.on("active", function (id) {
-            if (_waitingDocuments.hasOwnProperty(id)) {
-                return;
-            }
-
-            var documentPromise = _documentManager.getDocument(id);
-            
-            _waitingDocuments[id] = documentPromise;
-
-            documentPromise.done(function (document) {
-                delete _waitingDocuments[id];
-
-                if (_canceledDocuments.hasOwnProperty(id)) {
-                    delete _canceledDocuments[id];
-                } else {
-                    if (!_assetManagers.hasOwnProperty(id)) {
-                        _assetManagers[id] = new AssetManager(generator, config, document, _renderManager);
-
-                        document.on("close", function () {
-                            _assetManagers[id].pause();
-                            delete _assetManagers[id];
-                        }.bind(this));
-                    }
-                    _assetManagers[id].unpause();
-                }
-            });
-        });
-
-        _stateManager.on("inactive", function (id) {
-            if (_waitingDocuments.hasOwnProperty(id)) {
-                _canceledDocuments[id] = true;
-            } else {
-                _assetManagers[id].pause();
-            }
-        });
+        _stateManager.on("active", _enableAssetGeneration);
+        _stateManager.on("inactive", _disableAssetGeneration);
     }
 
     exports.init = init;
